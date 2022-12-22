c49d0ff5523ff9f16c554df2d14312825c81376f
Mixail132 <millionbaxov@gmail.com>
2022-12-22 16:57:53 +0300

    Doesn't double the data

--- archives/urls.py
+++ archives/urls.py
@@ -4,8 +4,12 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("add/", views.add, name="add"),
-    path("add/addrecord/", views.addrecord, name="addrecord"),
-    path("update/<int:id>", views.update, name="update"),
+    path("del/", views.delete, name="del"),
+    path("del/delrecord/",  views.delrecord, name="delrecord"),
+    path("add/addrecord/",  views.addrecord, name="addrecord"),
+    path("update/<int:id>", views.update,    name="update"),
     #path("update/updaterecord/<int:id>", views.updaterecord, name="updaterecord"),
     path("delete/<int:id>", views.delete, name="delete"),
+    path("deleteselected/", views.deleteselected, name="deleteselected"),
+    path("deletestring/<int:id>", views.deletestring, name="deletestring"),
 ]
--- archives/views.py
+++ archives/views.py
@@ -4,8 +4,7 @@ from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Archives
 import requests
-
-
+from datetime import timedelta, date
 def index(request):
     myarchives = Archives.objects.all().values()
     template = loader.get_template("archives.html")
@@ -17,6 +16,9 @@ def add(request):
     template = loader.get_template("add.html")
     return HttpResponse(template.render({}, request))
 
+def delete(request):
+    template = loader.get_template("delete.html")
+    return HttpResponse(template.render({}, request))
 
 def update(request,id):
     myarchives = Archives.objects.get(id=id)
@@ -49,22 +51,74 @@ def update(request,id):
 #     context = {"myarchives": myarchives,}
 #     return HttpResponse(template.render(context, request))
 
-def delete(request,id):
-    myarchives = Archives.objects.get(id=id)
-    myarchives.usd = 0
-    myarchives.eur = 0
-    myarchives.rub = 0
+def delrecord(request):
+    sdat = request.POST["sdat"]
+    fdat = request.POST["fdat"]
+    daterange = date_range(sdat, fdat)
+    for i in daterange:
+           myarchives = Archives.objects.get(dat=i)
+           myarchives.delete()
+
+
+        #Archives(dat=i, usd = 0, eur = 0, rub = 0)
+        #data.save()
+
+    # myarchives = Archives.objects.get(id=id)
+    # myarchives.usd = 0
+    # myarchives.eur = 0
+    # myarchives.rub = 0
     #myarchives.delete()
-    myarchives.save()
+    # myarchives.save()
     return HttpResponseRedirect(reverse("index"))
 
+def deletestring(request,id):
+    myarchives = Archives.objects.get(id=id)
+    myarchives.delete()
+    return HttpResponseRedirect(reverse ("index"))
 
 def addrecord(request):
-    d = request.POST["dat"]
-    u = request.POST["usd"]
-    e = request.POST["eur"]
-    dat = Archives(dat=d, usd=u, eur=e)
-    dat.save()
+    sdat = request.POST["sdat"].split("-")
+    fdat = request.POST["fdat"].split("-")
+    # получили из полей <input > формы HTML с именами fdat и sdat
+    # значения ввиде строки через дефис ГГГГ-ММ-ДД и сделали его списком
+    sdate = date(int(sdat[0]),int(sdat[1]),int(sdat[2]))
+    fdate = date(int(fdat[0]),int(fdat[1]),int(fdat[2]))
+    # превратили каждый элемент списка в число и передали в функцию
+    # date (как она того требует) библиотеки datetime
+    def daterange(startdate, finaldate):
+        for n in range(int((finaldate-startdate).days)+1):
+            yield startdate + timedelta(n)
+
+    m = Archives.objects.values() # m - список словарей
+    for dates in daterange(sdate, fdate):
+        for i in range(len(m)):
+            if dates == m[i]["dat"]: # i  элемент списка по порядку, m - ключ вложенного словаря
+                break
+        else: # работает только после break
+            t = dates.strftime("%Y-%m-%d") #  делаем из даты строку
+            data = Archives(dat=t, usd=0, eur=0, rub=0)
+            data.save()
+
+
+    # for i in Archives.objects.all():
+    #     n = Archives.dat
+    #     counter = 0
+    #     for j in Archives.objects.all():
+    #         h = Archives.dat
+    #         if n == h:
+    #             counter +=1
+    #         if counter > 1:
+    #             m = Archives.objects.get(id=id)
+    #             m.delete()
+    #
+
+    # cusd = 0 if request.POST["cusd"] == "on" else None
+    # ceur = 0 if request.POST["ceur"] == "on" else None
+    # crub = 0 if request.POST["crub"] == "on" else None
+    # print(cusd)
+    # print(ceur)
+    # dat = Archives(dat=d, usd=u, eur=e)
+    # dat.save()
     return HttpResponseRedirect(reverse("index"))
 
 
@@ -79,3 +133,16 @@ def updaterecord(request, id):
     data.save()
     return HttpResponseRedirect(reverse("index"))
 
+def deleteselected (request, id):
+    myarchives = Archives.objects.get(id=id)
+    myarchives.delete()
+    #Archives(dat=i, usd = 0, eur = 0, rub = 0)
+        #data.save()
+
+    # myarchives = Archives.objects.get(id=id)
+    # myarchives.usd = 0
+    # myarchives.eur = 0
+    # myarchives.rub = 0
+    #myarchives.delete()
+    # myarchives.save()
+    return HttpResponseRedirect(reverse("index"))
