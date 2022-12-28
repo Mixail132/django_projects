7b516e3d70fba8faa8af172a7b53eab8237da6f8
Mixail132 <millionbaxov@gmail.com>
2022-12-28 16:33:51 +0300

    Add connection test

--- archives/tests.py
+++ archives/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+#from django.test import TestCase
+import requests
+import time
+from datetime import datetime, timedelta
+def connection ():
+    """Протестировать длительность соединения с конкретным API"""
+    now_time = time.time()
+    yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+    requests.get(f"https://www.nbrb.by/api/exrates/rates/USD?parammode=2&ondate={yesterday}")
+    requests.get(f"https://www.nbrb.by/api/exrates/rates/EUR?parammode=2&ondate={yesterday}")
+    requests.get(f"https://www.nbrb.by/api/exrates/rates/RUB?parammode=2&ondate={yesterday}")
+    dif_time = time.time() - now_time
+    return dif_time
 
-# Create your tests here.
+assert connection() < 20, "Connection time is too long, the remote server does not answer!"
+
+
